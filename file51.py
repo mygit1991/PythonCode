27b08fa1f30dfc3740ceda851e128bd4654d4c38
mygit1991 <76545835+mygit1991@users.noreply.github.com>
2021-01-01 02:08:28 +0530

    Add files via upload

---
+++ file51.py
@@ -0,0 +1,17 @@
+A movie theater charges different ticket prices depending on a person’s age. If a person is under the age of 3, the ticket is free; if they are between 3 and 12, the ticket is $10; and if they are over age 12, the ticket is $15. Write a loop in which you ask users their age, and then tel them the cost of their movie ticket.
+
+prompt = "How old are you?"
+prompt += "\nEnter 'quit' when you are finished. "
+
+while True:
+    age = input(prompt)
+    if age == 'quit':
+        break
+    age = int(age)
+
+    if age < 3:
+        print("  Ticket is free!")
+    elif age < 13:
+        print("ticket is $10.")
+    else:
+        print(" ticket is $15.")
